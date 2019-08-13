49fbc717de76de4edf55c4b1615e7ac242bdac6f
rezaneo7 <ramazanpoorreza@gmail.com>
2019-08-12 23:11:54 -0700

    add files

---
+++ src/wifi_functions.h
@@ -0,0 +1,10 @@
+#ifndef _WIFI_FUNCTIONS_H_
+#define _WIFI_FUNCTIONS_H_
+
+#define WIFI_SSID			"reza"
+#define WIFI_PASS			"rezaneo7"
+
+void wifi_initialise(void);
+void wifi_wait_connected();
+
+#endif
